16ef532316298c82a44a8b69fc88cbad9d6a78ff
Rajababu Shah <rajababushah.in@gmail.com>
2024-03-26 10:47:14 +0545

    database Removed

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,9 @@
 from django.http import HttpResponse
-from .models import Question
 from django.template import loader
 
 def index(request):
-    myqn = Question.objects.all().values()
     template = loader.get_template('index.html')
     context = {
-        'mydata': myqn,
+        'mydata': "Hello",
     }
     return HttpResponse(template.render(context, request))
